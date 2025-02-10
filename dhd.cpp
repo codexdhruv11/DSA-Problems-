62f9e543d9c7455791841d01b2f80dffc227c7d1
codexdhruv11 <dhruvkaushikjee9457@gmail.com>
2025-02-10 14:26:40 +0530

    Add files via upload
    CODEFORCES 1809 B
    https://codeforces.com/contest/1809/problem/B

---
+++ dhd.cpp
@@ -0,0 +1,189 @@
+/*DHRUV KAUSHIK
+⢸⡄⠀⢦⡄⠀⠀⠀⠀⠀⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⡤⠔⠒⠂⠉⠉⠉⠉⠓⠒⠤⣄⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀
+⠀⠛⠠⣀⡴⠀⠀⠀⠀⠀⠀⠀⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⣠⠖⠚⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠢⣃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀
+⠀⠈⢁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⣀⡠⠴⠶⠒⠋⠁⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠈⠁⠦⠶⠶⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠤
+⠀⠀⠘⠿⠿⠦⠀⠀⡄⠀⠀⠀⠀⠀⠀⡜⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢇⣼⠛⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⢦⣀⠀⢸⣹⡀⠀⠀⣑⢦⡀⢤⡀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⡄⠀⠀⢈⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢰⣧⠀⠀⠀⣰⠃⠀⠀⢰⠀⡇⠀⢦⠀⠀⠀⠒⠦⣝⠳⣬⣧⢳⡀⠀⣿⣧⢷⠀⠳⡄⠀⠀⠀⠀⠈⠱⣄⠀⠀⠘⡄⠀⠀⠁⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠋⡆⠀⡴⢻⠀⡄⠀⢸⠀⡇⠂⠀⠳⣄⠀⠀⠀⠈⠙⠺⣿⣆⠹⣄⠀⠁⠈⡇⠀⠘⠆⠀⠀⠀⠀⠀⠹⣆⠀⠀⠹⡄⠀⠀⠸⡆⠀⠀⠀⡤⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⠆⡎⠀⠇⠀⢸⡇⢸⠠⡗⠀⠙⢾⠦⡀⠀⠀⠀⠀⠉⠲⣿⠃⠀⠀⣿⣆⠀⠀⠀⠀⠀⠀⡀⠀⠸⣦⠀⠀⢳⡀⠀⠀⢹⡄⠀⡸⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⠏⠀⡇⠀⠀⠀⡎⡇⢸⡄⣿⢦⠀⠈⠳⡌⠳⢄⡀⠀⠀⠀⠈⢷⣦⣤⠟⢮⣧⡀⠀⠀⠀⠀⢧⠀⠀⣿⡆⠀⠸⣷⠀⠀⠀⢣⡰⠃⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡏⡘⢰⡇⠀⠀⠀⢻⢇⢸⣧⢹⣮⡛⣄⠀⠙⣦⡈⢑⣦⣀⠀⠀⠀⠳⣝⢦⡄⠹⣧⠀⠀⠀⠀⠈⠀⡄⢸⣿⡀⠀⡟⣆⠀⠀⠟⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣴⡇⢸⡇⠀⠀⠀⢸⢸⠘⡟⣮⡇⠙⢮⡣⡀⠈⢿⣎⠳⣟⠳⡄⠀⠀⠈⣷⣟⣆⠈⢷⡀⠀⠀⠀⠀⢸⠀⠹⣧⠀⣧⢸⡆⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⣿⠀⠀⠇⢺⠀⠀⢸⡞⡆⢷⣘⣾⡴⠒⠛⢯⡀⠘⣏⠙⢾⣿⢾⣶⡀⠀⠘⢯⢺⡱⣄⢱⡀⠀⠀⠀⠈⡆⡆⢻⠀⣿⠀⢳⠀⠘⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⢹⣿⠀⠀⣸⡜⡆⠀⠈⣷⣿⢿⠉⣼⡿⣶⣶⣶⡿⢦⡸⣄⡞⠛⠷⣝⠿⣄⠀⠘⣆⠹⡜⢧⢻⡀⠀⠀⠀⣧⠃⠈⡇⡏⠀⠸⡄⢠⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⡄⣿⡇⠀⢧⣷⣳⡀⠀⠘⠸⣿⡀⠟⠀⠰⣿⡟⠁⠀⣹⣿⠃⠀⣠⠜⢳⡌⠳⣄⠹⡄⠙⠀⣳⣣⠀⠀⠀⣿⡇⠀⣧⠃⢰⠀⡇⢸⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠸⡇⣧⣹⣄⠸⣿⣿⣷⡄⠀⠀⢹⣇⠀⠀⠀⠉⠀⠀⣼⡟⠉⣧⠞⠁⣠⠋⠀⡀⣨⢧⡇⠀⣰⢫⡿⣇⠀⠀⣿⠃⠀⢸⠀⢸⡇⢣⡏⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣸⢻⣞⢦⣿⣷⣏⢿⣦⡀⠀⢻⡀⠀⠀⠀⠀⡜⡽⣠⣾⠃⠀⠀⠀⢠⣾⡿⠁⠀⢉⣶⡷⣋⠀⡼⣴⢸⢸⡄⠀⠏⠀⢸⡧⡾⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡇⠙⣆⠙⠿⣬⡟⠙⡟⠳⣤⣳⡀⠀⠀⠀⢱⠟⢱⠃⠀⠀⠀⣰⠿⠋⠀⠀⣴⡿⢋⡞⠁⢸⡇⡸⡼⢸⠁⠀⠀⠀⢸⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⡀⠈⠆⠀⣿⡇⣰⠃⠀⠀⠈⠛⠀⠀⢠⠏⢠⠏⠀⢀⣠⣾⡷⠂⢀⣠⠞⣡⡴⠋⠀⢠⡿⠁⣷⡇⠀⠀⠀⢀⡤⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠤⠤⠽⠯⠭⢸⣻⡽⣧⡀⠀⠀⠀⠀⠀⠀⠀⢠⠃⢀⡴⢫⣿⠟⠡⠔⢋⣤⠞⢁⣴⠃⢀⣿⠃⠀⡟⠀⢠⠟⠀⢸⣱⠣⠤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⡇⣇⢫⠹⣷⡀⣠⡴⠖⠂⢠⣿⠞⠉⡴⣻⠃⢀⣤⠞⣿⠃⡴⣿⠇⠀⡼⢁⡀⣼⡅⣠⠇⢠⣶⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⡇⠘⢿⡄⢻⡟⢦⡀⠤⠶⠋⠀⠀⠀⣼⡷⠚⠉⢀⡜⣿⠞⢡⠟⢀⣾⣵⡿⠀⣽⣱⠇⣠⣿⣻⠃⠀⠀⠀⠀⠀⣀⡤⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⠀⠈⠳⣌⢿⣣⠙⣆⠀⠀⠀⠀⠈⠀⣀⣤⠞⠉⠸⠋⢠⣞⠴⢻⠟⣸⡷⡾⠋⣿⡼⠁⠏⠋⠀⠀⢀⣀⠶⠋⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣷⠀⠀⠀⠘⢿⣏⣷⣌⢳⣀⣀⣾⢟⠛⠉⣿⣀⣀⣠⣤⣼⣥⣤⣶⣾⣧⣤⣤⣄⣸⠁⠀⠀⠀⣠⣴⣿⣖⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⡀⠀⢀⣀⠽⠛⠚⠦⠥⣜⣟⣿⡳⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣷⣦⣶⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠖⠋⠀⠀⠀⠀⠀⠀⠀⠉⠉⠓⢻⣿⠟⢻⠉⠉⠁⠀⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⡿⠛⠉⠀⠹⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢾⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⢸⠃⠀⠀⠀⣠⣾⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⡀⠀⠀⠀⠀⠀⠈⠳⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⢽⡄⠀⠀⠈⢿⣿⣿⠿⠋⢀⣤⣦⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠇⠀⠀⠀⠀⠀⠀⢀⡿⣿⡛⣿⡄⠀⠀⠀⠀⠀⠀⠘⣿⠳⣤⠤⠤⠔⠚⠋⠁⠀⠀⠀⣿⣿⡆⠀⠀⠈⠋⠀⠀⢺⣿⣿⣿⣧⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠀⠀⠀⠀⠀⢀⣴⢻⣿⠹⣿⣿⡇⠀⠀⠀⣀⣀⣀⣤⡼⠆⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⠗⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣧⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⡇⠀⠀⠀⠀⠀⠁⠸⣄⠙⠷⡿⠟⠁⠀⠀⠈⣿⠉⠀⠀⠀⢴⡤⢬⡇⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡀⠀⠀⠀⣀⣤⣾⣿⣿⣿⣿⣿⣷⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠒⠀⠀⠀⠀⠀⢰⡟⠀⠀⠀⠀⣾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡟⢳⣠⣶⣾⣿⣿⣿⠟⠋⠙⣿⣿⣿⣷
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠇⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠈⣿⣿⡿⠟⠋⠀⠀⠀⠀⠘⠟⠋⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠌⣷⢻⢧⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<iterator>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<set>
+#include<bitset>
+#include<random>
+#include<map>
+#include<unordered_map>
+#include<stdio.h>
+#include<complex>
+#include<math.h>
+#include<cstring>
+#include<chrono>
+#include<string>
+using namespace std;
+#define DIVYA ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long
+#define lli long long
+#define umap unordered_map
+#define uset unordered_set
+#define lb lower_bound
+#define ub upper_bound
+#define en endl
+#define MAXIMUM max
+#define fo(i,a,b) for(i=a;i<=b;i++)
+#define all(v) (v).begin(),(v).end()
+#define all1(v) (v).begin()+1,(v).end()
+#define allr(v) (v).rbegin(),(v).rend()
+#define allr1(v) (v).rbegin()+1,(v).rend()
+#define sort0(v) sort(all(v))
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+#define pb push_back
+#define sz(x) (ll)x.size()
+#define ppb pop_back
+#define mkp make_pair
+#define inf 1000000000000000005
+#define LONG long long
+#define F first
+#define S second
+#define VLL vector<LONG>
+#define MLL map<LONG, LONG>
+#define LOOP(i, a, b) for(LONG i = a; i < b; i++)
+#define NEWLINE "\n"
+#define N 222222
+//#define s second
+//#define f first
+const ll mod = 1e9 + 7;
+
+ll inv(ll i) {if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+
+ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+
+ll mod_add(ll a, ll b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+
+ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
+vector<ll> sieve() {
+    vector<ll> primes;
+    bool a[1000000]={0};int mprimes = 1000000;a[0] = a[1] = true;for (int i = 2; i * i < mprimes; ++i)
+    {if (a[i] == false) {for (int j = i * i; j < mprimes; j = j + i){a[j] = true;}}}
+    for (int i = 2; i < mprimes; ++i){if (!a[i])primes.pb(i);}return primes;
+}
+ll get_diff(string s1, string s2)
+{
+    ll ans = 0;
+    ll i;
+    fo(i,0,sz(s1) - 1)
+    {
+        ans+=abs(s1[i] - s2[i]);
+    }
+    return ans;
+}
+void PrimeFactors(ll n) {
+    while (n % 2 == 0) {
+        cout << 2 << " ";
+        n = n / 2;
+    }
+
+    for (ll i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {cout << i << " ";n = n / i;}}
+    if (n > 2)
+        cout << n << " ";
+}
+int rightmostuppercase(const string& input, int endIndex) {
+    endIndex = min(endIndex, static_cast<int>(input.length()) - 1);
+    for (int i = endIndex; i >= 0; --i) {
+        if (isupper(input[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int rightmostlowercase(const string& input, int endIndex) {
+    endIndex = min(endIndex, static_cast<int>(input.length()) - 1);
+    for (int i = endIndex; i >= 0; --i) {
+        if (islower(input[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+int no_of_digits(long long n)
+{
+    string num = to_string(n);
+    return num.size();
+}
+//const int M = 1e6 , N= 1e3+20 , MOD = 1e9+7 , OO = 1e16;
+ll m ,n ,k , x, y,z, bla ,c=1,cnt ,ans ,ok ,a[N], vis[N] ;
+int BS(vector<ll>& A, ll d, int l, int r) {
+    int mid = (l+r) / 2;
+    //if(A[l] <= d && d < A[l+1]) return l;
+    if(A[mid] <= d && d < A[mid+1]) return mid;
+    else if(d < A[mid]) return BS(A, d, l, mid);
+    else return BS(A, d, mid+1, r);
+}
+void solve() {
+    ll n, res;
+    cin >> n;
+    res = sqrt(n);
+    if(res * res == n) res--;
+    cout << res << endl;
+}
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+    int t;
+    t=1;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+}
